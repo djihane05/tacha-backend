706362c0cd577ca16ea691e9ab53b1667c70b716
djihane05 <abderrahmanejihen05@gmail.com>
2025-05-28 15:02:44 +0200

    database

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
-const { Pool } = require("pg"); // Ajoute ça pour PostgreSQL
+const { Pool } = require("pg");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-
+console.log(process.env);
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+console.log(process.env);
 
 // Route de test
 app.get("/", (req, res) => {
@@ -29,7 +32,7 @@ app.get("/api/products", async (req, res) => {
     const { rows } = await pool.query("SELECT * FROM products");
     res.json(rows);
   } catch (error) {
-    console.error(error); // Ajoute ça pour voir l’erreur
+    console.error("Erreur dans /api/products :", error.message, error.stack);
     res.status(500).json({ error: "Erreur serveur" });
   }
 });
